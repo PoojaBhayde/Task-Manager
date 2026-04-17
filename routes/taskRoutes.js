54555c44c67608c01f490c8417b2df06ad56e47d
Pooja Bhayde <78949822+PoojaBhayde@users.noreply.github.com>
2026-04-17 13:01:09 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router2 = express.Router();
 const taskController = require("../controllers/taskController");
 const auth = require("../middleware/auth");
+const { validateTask } = require("../middleware/validation");
 
-router2.post("/", auth, taskController.createTask);
+router2.post("/", auth, validateTask, taskController.createTask);
 router2.get("/", auth, taskController.getTasks);
 router2.put("/:id", auth, taskController.updateTask);
 router2.delete("/:id", auth, taskController.deleteTask);
